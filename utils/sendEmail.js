2cdba0b376fd13e6fc9ac624583fefe54e442c2f
Amar ghaly <amargad0112030@gmil.com>
2026-05-05 13:32:45 +0300

    update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -8,7 +8,7 @@ export const sendEmail = async (to, otp) => {
     const transporter = nodemailer.createTransport({
       host: "smtp-relay.brevo.com",
       port: 587,
-      secure: false, 
+      secure: false,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
